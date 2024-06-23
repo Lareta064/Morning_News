ec304f3ebc0e7676dbb919d2aaa9c14226023501
Lareta064 <lareta064@gmail.com>
2024-06-23 22:29:14 +0300

    initial

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+	var headerMenuSlider = new Swiper('.header-menu-swiper', {
+		slidesPerView: 'auto',
+		spaceBetween: 40,
+		navigation:{
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+
+		}
+	});
+
+});

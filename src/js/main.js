5f600a9a4d208e015dfe632388d8f874b4937f84
Lareta064 <lareta064@gmail.com>
2024-06-25 09:25:55 +0300

    add-desk-custom-select

--- src/js/main.js
+++ src/js/main.js
@@ -80,4 +80,32 @@ document.addEventListener("DOMContentLoaded", function () {
 			}
 		});
 	}
+
+	/************* CUSTOM SELECT ************ */
+	document.querySelectorAll('.custom-select-wrapper').forEach(wrapper => {
+	wrapper.addEventListener('click', function() {
+		this.querySelector('.custom-select').classList.toggle('open');
+	});
+	});
+
+	// Добавление обработчика клика для всех .custom-option
+	document.querySelectorAll(".custom-option").forEach(option => {
+	option.addEventListener('click', function() {
+		let selected = this.parentNode.querySelector('.custom-option.selected');
+		if (selected) {
+		selected.classList.remove('selected');
+		}
+		this.classList.add('selected');
+		this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+	});
+	});
+
+	// Закрытие всех .custom-select, если клик был снаружи
+	window.addEventListener('click', function(e) {
+		document.querySelectorAll('.custom-select').forEach(select => {
+			if (!select.contains(e.target)) {
+			select.classList.remove('open');
+			}
+		});
+	});
 });
